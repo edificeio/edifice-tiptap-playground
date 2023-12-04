6680da03b02d867cda5d78256b971a337a6f0857
david-cc <david.caudeli@web-education.net>
2023-12-04 19:14:29 +0100

    fix: #WB-2011, Video upload returns an array of WorkspaceElement

--- frontend/src/components/Tiptap/Tiptap.tsx
+++ frontend/src/components/Tiptap/Tiptap.tsx
@@ -286,16 +286,19 @@ const Tiptap = () => {
           break;
         }
 
+        // Video type => result is of type WorkspaceElement[]
         case "video": {
-          const video = result as WorkspaceElement;
-          editor
-            ?.chain()
-            .focus()
-            .setVideo(
-              video._id || "",
-              `/workspace/document/${video._id}`,
-              true,
-            );
+          const videos = result as WorkspaceElement[];
+          videos.forEach((video) => {
+            editor
+              ?.chain()
+              .focus()
+              .setVideo(
+                video._id || "",
+                `/workspace/document/${video._id}`,
+                true,
+              );
+          });
           break;
         }
 
