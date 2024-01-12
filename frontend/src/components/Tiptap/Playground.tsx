ce42f93523ab6cfe11ed805a9976afe9563f70b5
Junior BERNARD <junior.bernard@opendigitaleducation.com>
2024-01-12 11:56:37 +0100

    feat: #WB-2473, convert old info and warning blocks

--- frontend/src/components/Tiptap/Playground.tsx
+++ frontend/src/components/Tiptap/Playground.tsx
@@ -8,6 +8,7 @@ const initialContent = `
 <h2>
   Hi there,
 </h2>
+<p class="info">Ceci est une information</p><p class="warning">Ceci est un avertissement</p>
 <p>
   this is a <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you'd probably expect from a text editor. But wait until you see the lists:
 </p>
